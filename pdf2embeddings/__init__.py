c6884a5acabb28148d44bfe328ffda88eb93f296
andrea-sottana-MoJ <55093726+andrea-sottana-MoJ@users.noreply.github.com>
2020-06-08 13:04:36 +0100

    minor updates

--- pdf2embeddings/__init__.py
+++ pdf2embeddings/__init__.py
@@ -1,1 +1,5 @@
+try:
+    from pdf2embeddings import scraper, arrange_text, embedder, process_user_queries
+except ModuleNotFoundError as err:
+    print(f"You have not imported all the required packages. \n{err}")
 __version__ = '0.1.0'
