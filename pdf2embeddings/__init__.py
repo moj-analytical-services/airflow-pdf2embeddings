95f18f76002546fcc5875c03220266c563769e85
andrea-sottana-MoJ <55093726+andrea-sottana-MoJ@users.noreply.github.com>
2020-09-17 17:31:31 +0100

    version bump

--- pdf2embeddings/__init__.py
+++ pdf2embeddings/__init__.py
@@ -2,4 +2,4 @@ try:
     from pdf2embeddings import scraper, arrange_text, embedder, process_user_queries
 except ModuleNotFoundError as err:
     print(f"You have not imported all the required packages. \n{err}")
-__version__ = '0.2.5'
+__version__ = '0.2.6'
